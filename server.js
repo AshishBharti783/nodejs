2e3b79e67854181e5120d18f50693de4c7ba2611
AshishBharti783 <71437783+AshishBharti783@users.noreply.github.com>
2023-07-24 15:25:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World And Welcome to Nodejs :)');
+  res.send('Hello World And Welcome to integration of jenkins :)');
 });
 
 app.listen(PORT, HOST, () => {
